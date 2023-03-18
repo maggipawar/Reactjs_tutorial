469af5bf864262c770f2e94df233c5163a70e3f6
Ganesh <ganeshnpawar1987@gmail.com>
2023-03-19 01:12:59 +0530

    Event with functional component Example

--- src/User.js
+++ src/User.js
@@ -1,9 +1,13 @@
-import { Component } from "react";
-
-export class User extends Component{
-    render(){
-        return(
-            <h1>User Class component</h1>  // JSX element
-        )
+export function User(){
+    let data = 'Ganesh';
+    function apple(){
+        data = 'Pawar';
+        alert(data);
     }
+    return(
+        <div>
+            <p>User component</p>
+            <button onClick={apple}>Click Me</button>
+        </div>
+    )
 }
