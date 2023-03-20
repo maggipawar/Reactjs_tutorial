23e71ea4e583449dc9087e9fba39b9070d8584a4
Ganesh <ganeshnpawar1987@gmail.com>
2023-03-20 14:58:51 +0530

    worked on form element with functional component Example

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 // import logo from "./logo.svg";
 // import "./App.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function App() {
-  const [status,setStatus]=useState(true);
-  
+  const [name, setName] = useState(null);
+  const [slt, setSlt] = useState(null);
+  const [tnc, setTnc] = useState(false);
+  // const [clear,setClear]=useState(false);
+
+  function getData(e) {
+    e.preventDefault();
+    console.log(name, slt, tnc);
+  }
   return (
     <div>
-    {
-      status ? <h1>Hello World</h1>: null
-    }
-      {/* <button onClick={()=> setStatus(false)}>Hide</button>
-      <button onClick={()=> setStatus(true)}>Show</button> */}
-      
-      <button onClick={()=> setStatus(!status)}>Toggle</button>
+      <h1>Working with form elements</h1>
+      <form onSubmit={getData}>
+        {/* <h2>{name} {slt} {tnc}</h2> */}
+        <input
+          type="text"
+          placeholder="Enter Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br /> <br />
+        <select onChange={(e) => setSlt(e.target.value)}>
+          <option>Select Option</option>
+          <option>Javascript</option>
+          <option>HTML</option>
+          <option>CSS</option>
+        </select>
+        <br /> <br />
+        <input type="checkbox" onChange={(e) => setTnc(e.target.checked)} />
+        <span>Accept term and conditons</span> <br /> <br />
+        <button type="submit">Submit</button>
+        {/* <button onClick={}>Clear</button> */}
+      </form>
     </div>
   );
 }
