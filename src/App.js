508286be6a4dbdc1e3fb3d5988ace5b5bfa347ac
Ganesh <ganeshnpawar1987@gmail.com>
2023-03-20 18:52:51 +0530

    render method with class component Example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 // import logo from "./logo.svg";
 // import "./App.css";
 import { User } from "./User";
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
 function App() {
-  const [login, setLogin] = useState(1);
+  // const [name, setName] = useState('Ganesh');
   return (
     <div>
     <h1>App Component</h1>
     <User />
+    {/* <button onClick={()=> setName('Pawar')}>Update Props</button> */}
     </div>
   );
 }
--- src/User.js
+++ src/User.js
@@ -1,52 +1,18 @@
-import React, { useState } from "react";
-
-export const User = () => {
-  const [user,setUser]=useState(null);
-  const [pass,setPass]=useState(null);
-  const [userVal,setUserVal]=useState(false);
-  const [passVal,setPassVal]=useState(false);
-
-  function submitHandler(e){
-    if(user.length<3 || pass.length<3){
-      alert('Type Correct Value');
-    }else{
-      alert('All is Good :)');
+import React, { Component } from 'react';
+export class User extends Component{
+  constructor(){
+    super();
+    this.state={
+      email:'Laxmi@gmail.com'
     }
-
-    e.preventDefault();    
-
   }
-
-  function userHandler(e){
-    let item = e.target.value;
-    if(item.length<3){
-      setUserVal(true);
-    }else{
-      setUserVal(false);
-    }
-    setUser(item);
-
+  render(){
+    console.log(`Render with State', ${this.state.email}`);
+    return(
+      <div>
+        <h1>User component </h1>
+        <button onClick={()=> this.setState({email:'Ganesh@gmail.com'})}>Update State</button> 
+      </div>
+    )
   }
-
-  function passHandler(e){
-    let item = e.target.value;
-    if(item.length<3){
-      setPassVal(true);
-    }else{
-      setPassVal(false);
-    }
-    setPass(item);
-  }
-
-  return (
-    <div> 
-    <form onSubmit={submitHandler}>
-      <input type='text' placeholder="Enter User ID" onChange={userHandler}/>{userVal ?<span>User Not valid</span>:null}
-      <br/> <br/>
-      <input type='password' placeholder="Enter User Password" onChange={passHandler} />{passVal ?<span>Password Not valid</span>:null}
-      <br/> <br/>
-      <button type='submit'>Submit</button>
-    </form>
-    </div>
-  );
-};
+}
