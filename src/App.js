322bc1248fd9e9cd6dba50728f85f5de231bf1a0
Ganesh <ganeshnpawar1987@gmail.com>
2023-03-20 17:17:46 +0530

    basic validation on form with functional component Example

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 // import logo from "./logo.svg";
 // import "./App.css";
+import { User } from "./User";
 import React, { useState } from "react";
 
 function App() {
   const [login, setLogin] = useState(1);
   return (
     <div>
-      <h1>Working with Conditional rendering</h1>
-
-      {login == 1 ? (
-        <h1>Welcome Ganesh</h1>
-      ) : login == 2 ? (
-        <h1>Welcome Guest</h1>
-      ) : (
-        <h1>Welcome User</h1>
-      )}
+    <h1>App Component</h1>
+    <User />
     </div>
   );
 }
--- src/User.js
+++ src/User.js
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const User = (props) => {
+export const User = () => {
+  const [user,setUser]=useState(null);
+  const [pass,setPass]=useState(null);
+  const [userVal,setUserVal]=useState(false);
+  const [passVal,setPassVal]=useState(false);
+
+  function submitHandler(e){
+    if(user.length<3 || pass.length<3){
+      alert('Type Correct Value');
+    }else{
+      alert('All is Good :)');
+    }
+
+    e.preventDefault();    
+
+  }
+
+  function userHandler(e){
+    let item = e.target.value;
+    if(item.length<3){
+      setUserVal(true);
+    }else{
+      setUserVal(false);
+    }
+    setUser(item);
+
+  }
+
+  function passHandler(e){
+    let item = e.target.value;
+    if(item.length<3){
+      setPassVal(true);
+    }else{
+      setPassVal(false);
+    }
+    setPass(item);
+  }
+
   return (
-    <div style={{ background: "green", padding: "10px" }}>
-      <h1>User Name is:{props.Name} </h1>
-      <h2>Email is:{props.email} </h2>
-      <h4>Address is:{props.other.address}</h4>
+    <div> 
+    <form onSubmit={submitHandler}>
+      <input type='text' placeholder="Enter User ID" onChange={userHandler}/>{userVal ?<span>User Not valid</span>:null}
+      <br/> <br/>
+      <input type='password' placeholder="Enter User Password" onChange={passHandler} />{passVal ?<span>Password Not valid</span>:null}
+      <br/> <br/>
+      <button type='submit'>Submit</button>
+    </form>
     </div>
   );
 };
