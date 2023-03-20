d66037924b18d26048ca64af0cf60a055e0f7350
Ganesh <ganeshnpawar1987@gmail.com>
2023-03-21 01:58:51 +0530

    Array with map() in functional component Example

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 // import logo from "./logo.svg";
 // import "./App.css";
-import { User } from "./User";
-import { useState } from "react";
-
+import React from "react";
 function App() {
-  const [count, setCount] = useState(0);
-  const [data,setData]=useState(0);
-  
+  const students = [
+    { name: "Ganesh", email: "pawar@gmail.com", contact: 555 },
+    { name: "Laxmi", email: "laxmi@gmail.com", contact: 222 },
+    { name: "Vaishali", email: "Vaishali@gmail.com", contact: 333 },
+    { name: "pawar", email: "pawar@gmail.com", contact: 444 },
+  ];
+
   return (
-    <div>
-    <User count={count} data={data} />
-    <button onClick={()=> setCount(count+1)}>Update Count</button>
-    <button onClick={()=> setData(data+1)}>Update Data</button>
-    </div>
+    <table border="1">
+      {students.map((data, i) => {
+        return (
+          <tr>
+            <td key={i}>{i + 1}</td>
+            <td>{data.name} </td>
+            <td>{data.email} </td>
+            <td>{data.contact}</td>
+          </tr>
+        );
+      })}
+    </table>
   );
 }
 
